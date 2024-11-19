0267ec9b8752b44f142bf6747116c4d00c92f3a8
staffy31 <mounkubito@gmail.com>
2024-11-19 10:17:18 +0200

    purchase report

--- ui/php/report.php
+++ ui/php/report.php
@@ -20,19 +20,29 @@ if(isset($_GET['action']) && $_GET['action'] == "sales")
     </thead>
     <tbody>
     <?php
-    require "db_connection.php";
-    if($con) {
+    $jsonFilePath = '../data/purchases.json';
+    if (file_exists($jsonFilePath)) {
+
       $seq_no = 0;
       $total = 0;
-      if($start_date == "" || $end_date == "")
-        $query = "SELECT * FROM purchases";
-      else
-        $query = "SELECT * FROM purchases WHERE PURCHASE_DATE BETWEEN '$start_date' AND '$end_date'";
-      $result = mysqli_query($con, $query);
-      while($row = mysqli_fetch_array($result)) {
-        $seq_no++;
-        showPurchaseRow($seq_no, $row);
-        $total = $total + $row['TOTAL_AMOUNT'];
+      $jsonData = file_get_contents($jsonFilePath); 
+      $purchases = json_decode($jsonData, true);    
+      if ($purchases === null) {
+        echo "Error: Unable to parse JSON file.";
+      } else {
+        foreach ($purchases as $purchase) {
+          if (!empty($start_date) && !empty($end_date)) {
+            if ($purchase['PURCHASE_DATE'] >= $start_date && $purchase['PURCHASE_DATE'] <= $end_date) {
+              $seq_no++;
+              showPurchaseRow($seq_no, $purchase);
+              $total += $purchase['TOTAL_AMOUNT'];
+            }
+          } else {
+            $seq_no++;
+            showPurchaseRow($seq_no, $purchase);
+            $total += $purchase['TOTAL_AMOUNT'];
+          }
+        }
       }
       ?>
       </tbody>
@@ -45,4 +55,69 @@ if(isset($_GET['action']) && $_GET['action'] == "sales")
       <?php
     }
   }
+
+  function showPurchaseRow($seq_no, $row) {
+    ?>
+    <tr>
+      <td><?php echo $seq_no; ?></td>
+      <td><?php echo $row['PURCHASE_DATE']; ?></td>
+      <td><?php echo $row['VOUCHER_NUMBER']; ?></td>
+      <td><?php echo $row['ID']; ?></td>
+      <td><?php echo $row['SUPPLIER_NAME'] ?></td>
+      <td><?php echo $row['TOTAL_AMOUNT']; ?></td>
+    </tr>
+    <?php
+  }
   
+  function showSales($start_date, $end_date) {
+    ?>
+    <thead>
+      <tr>
+        <th>SL</th>
+        <th>Sales Date</th>
+        <th>Invoice Number</th>
+        <th>Customer Name</th>
+        <th>Total Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+    require "db_connection.php";
+    if($con) {
+      $seq_no = 0;
+      $total = 0;
+      if($start_date == "" || $end_date == "")
+        $query = "SELECT * FROM invoices INNER JOIN customers ON invoices.CUSTOMER_ID = customers.ID";
+      else
+        $query = "SELECT * FROM invoices INNER JOIN customers ON invoices.CUSTOMER_ID = customers.ID WHERE INVOICE_DATE BETWEEN '$start_date' AND '$end_date'";
+      $result = mysqli_query($con, $query);
+      while($row = mysqli_fetch_array($result)) {
+        $seq_no++;
+        //print_r($row);
+        showSalesRow($seq_no, $row);
+        $total = $total + $row['NET_TOTAL'];
+      }
+      ?>
+      </tbody>
+      <tfoot class="font-weight-bold">
+        <tr style="text-align: right; font-size: 24px;">
+          <td colspan="4" style="color: green;">&nbsp;Total Sales =</td>
+          <td class="text-primary"><?php echo $total; ?></td>
+        </tr>
+      </tfoot>
+      <?php
+    }
+  }
+  
+  function showSalesRow($seq_no, $row) {
+    ?>
+    <tr>
+      <td><?php echo $seq_no; ?></td>
+      <td><?php echo $row['INVOICE_DATE']; ?></td>
+      <td><?php echo $row['INVOICE_ID']; ?></td>
+      <td><?php echo $row['NAME']; ?></td>
+      <td><?php echo $row['NET_TOTAL'] ?></td>
+    </tr>
+    <?php
+  }
+  ?>
---
+++ ui/purchase_report.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Dashboard - Home</title>
+  <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  <script src="../bootstrap/js/jquery.min.js"></script>
+  <script src="../bootstrap/js/bootstrap.min.js"></script>
+  <link href="../dist/css/icons/font-awesome/css/fontawesome-all.min.css" rel="stylesheet">
+  <link href="../dist/css/icons/material-design-iconic-font/css/materialdesignicons.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="../dist/css/icons/themify-icons/themify-icons.css" rel="stylesheet">
+  <link rel="stylesheet" href="../css/sidenav.css">
+  <link rel="stylesheet" href="../css/home.css">
+  <script type="text/javascript" src="../js/suggestions.js"></script>
+  <script type="text/javascript" src="../js/new_purchase.js"></script>
+  <script src="../js/validateForm.js"></script>
+
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      background-image: url('../images/bg.png');
+      background-size: cover;
+      background-attachment: fixed;
+      font-family: 'Arial', sans-serif;
+    }
+  </style>
+</head>
+
+<body>
+
+  <?php include "../menu/side/sections/sidenav.html"; ?>
+  <div class="container-fluid">
+    <div class="container">
+      <!-- header section -->
+      <?php
+      require "../php/header.php";
+      createHeader('book', 'Purchase Report', 'Showing Purchase Report');
+      ?>
+      <!-- header section end -->
+
+      <!-- form content -->
+      <div class="row">
+
+        <div class="col-md-12 form-group form-inline">
+          <label class="font-weight-bold" for="">Start Date :&emsp;</label>
+          <input type="date" class="form-control" id="start_date" onchange="showReport('purchase');">
+          &emsp;
+          <label class="font-weight-bold" for="">End Date :&emsp;</label>
+          <input type="date" class="form-control" id="end_date" onchange="showReport('purchase');">
+          &emsp;
+          <button class="btn btn-success" onclick="location.reload();"><i class="fa fa-refresh"></i></button>
+        </div>
+
+        <div class="col col-md-12">
+          <hr class="col-md-12" style="padding: 0px; border-top: 2px solid  #02b6ff;">
+        </div>
+
+        <div class="col col-md-12 table-responsive">
+          <div id="print_content" class="table-responsive">
+            <table class="table table-bordered table-striped table-hover" id="purchase_report_div">
+              <?php
+              require "php/report.php";
+              showPurchases("", "");
+              ?>
+            </table>
+          </div>
+        </div>
+
+        <div class="col-md-12 text-center">
+          <button class="btn btn-primary" onclick="printReport('Purchase');">Print</button>
+        </div>
+
+      </div>
+      <!-- form content end -->
+    </div>
+  </div>
+</body>
+
+</html>
