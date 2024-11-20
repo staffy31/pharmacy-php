e08ccca00de549b8d59c6f73a9e13a757c1407dd
staffy31 <mounkubito@gmail.com>
2024-11-20 09:10:26 +0200

    sales report

--- ui/php/report.php
+++ ui/php/report.php
@@ -1,32 +1,33 @@
 <?php
 
-if(isset($_GET['action']) && $_GET['action'] == "purchase")
+if (isset($_GET['action']) && $_GET['action'] == "purchase")
   showPurchases($_GET['start_date'], $_GET['end_date']);
 
-if(isset($_GET['action']) && $_GET['action'] == "sales")
+if (isset($_GET['action']) && $_GET['action'] == "sales")
   showSales($_GET['start_date'], $_GET['end_date']);
 
-  function showPurchases($start_date, $end_date) {
-    ?>
-    <thead>
-      <tr>
-        <th>SL</th>
-        <th>Purchase Date</th>
-        <th>Voucher Number</th>
-        <th>Invoice No</th>
-        <th>Supplier Name</th>
-        <th>Total Amount</th>
-      </tr>
-    </thead>
-    <tbody>
+function showPurchases($start_date, $end_date)
+{
+  ?>
+  <thead>
+    <tr>
+      <th>SL</th>
+      <th>Purchase Date</th>
+      <th>Voucher Number</th>
+      <th>Invoice No</th>
+      <th>Supplier Name</th>
+      <th>Total Amount</th>
+    </tr>
+  </thead>
+  <tbody>
     <?php
     $jsonFilePath = '../data/purchases.json';
     if (file_exists($jsonFilePath)) {
 
       $seq_no = 0;
       $total = 0;
-      $jsonData = file_get_contents($jsonFilePath); 
-      $purchases = json_decode($jsonData, true);    
+      $jsonData = file_get_contents($jsonFilePath);
+      $purchases = json_decode($jsonData, true);
       if ($purchases === null) {
         echo "Error: Unable to parse JSON file.";
       } else {
@@ -54,9 +55,10 @@ if(isset($_GET['action']) && $_GET['action'] == "sales")
       </tfoot>
       <?php
     }
-  }
+}
 
-  function showPurchaseRow($seq_no, $row) {
+  function showPurchaseRow($seq_no, $row)
+  {
     ?>
     <tr>
       <td><?php echo $seq_no; ?></td>
@@ -68,8 +70,9 @@ if(isset($_GET['action']) && $_GET['action'] == "sales")
     </tr>
     <?php
   }
-  
-  function showSales($start_date, $end_date) {
+
+  function showSales($start_date, $end_date)
+  {
     ?>
     <thead>
       <tr>
@@ -81,21 +84,41 @@ if(isset($_GET['action']) && $_GET['action'] == "sales")
       </tr>
     </thead>
     <tbody>
-    <?php
-    require "db_connection.php";
-    if($con) {
-      $seq_no = 0;
-      $total = 0;
-      if($start_date == "" || $end_date == "")
-        $query = "SELECT * FROM invoices INNER JOIN customers ON invoices.CUSTOMER_ID = customers.ID";
-      else
-        $query = "SELECT * FROM invoices INNER JOIN customers ON invoices.CUSTOMER_ID = customers.ID WHERE INVOICE_DATE BETWEEN '$start_date' AND '$end_date'";
-      $result = mysqli_query($con, $query);
-      while($row = mysqli_fetch_array($result)) {
-        $seq_no++;
-        //print_r($row);
-        showSalesRow($seq_no, $row);
-        $total = $total + $row['NET_TOTAL'];
+      <?php
+      $jsoninvoices = '../data/invoices.json';
+      $jsoncustomers = '../data/customers.json';
+
+      $invoices = json_decode(file_get_contents($jsoninvoices), true);
+      $customers = json_decode(file_get_contents($jsoncustomers), true);
+
+      if ($invoices && $customers) {
+        $seq_no = 0;
+        $total = 0;
+        $filteredInvoices = [];
+        if (!empty($start_date) && !empty($end_date)) {
+          foreach ($invoices as $invoice) {
+            if ($invoice['INVOICE_DATE'] >= $start_date && $invoice['INVOICE_DATE'] <= $end_date) {
+              $filteredInvoices[] = $invoice;
+            }
+          }
+        } else {
+          $filteredInvoices = $invoices;
+        }
+
+        $joinedData = [];
+        foreach ($filteredInvoices as $invoice) {
+          foreach ($customers as $customer) {
+            if ($invoice['CUSTOMER_ID'] == $customer['ID']) {
+              $joinedData[] = array_merge($invoice, $customer);
+            }
+          }
+        }
+        foreach ($joinedData as $row) {
+          $seq_no++;
+          // $total += $row['AMOUNT'];
+          showSalesRow($seq_no, $row);
+          $total = $total + $row['NET_TOTAL'];
+        }
       }
       ?>
       </tbody>
@@ -105,19 +128,20 @@ if(isset($_GET['action']) && $_GET['action'] == "sales")
           <td class="text-primary"><?php echo $total; ?></td>
         </tr>
       </tfoot>
-      <?php
-    }
-  }
-  
-  function showSalesRow($seq_no, $row) {
-    ?>
-    <tr>
-      <td><?php echo $seq_no; ?></td>
-      <td><?php echo $row['INVOICE_DATE']; ?></td>
-      <td><?php echo $row['INVOICE_ID']; ?></td>
-      <td><?php echo $row['NAME']; ?></td>
-      <td><?php echo $row['NET_TOTAL'] ?></td>
-    </tr>
     <?php
   }
-  ?>
+  // }
+
+  function showSalesRow($seq_no, $row)
+  {
+    ?>
+      <tr>
+        <td><?php echo $seq_no; ?></td>
+        <td><?php echo $row['INVOICE_DATE']; ?></td>
+        <td><?php echo $row['ID']; ?></td>
+        <td><?php echo $row['NAME']; ?></td>
+        <td><?php echo $row['NET_TOTAL'] ?></td>
+      </tr>
+    <?php
+  }
+?>
